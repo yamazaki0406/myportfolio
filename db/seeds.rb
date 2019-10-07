f86cde3f23f6f4c7e4fe3cda2c4056e2ba939204
Yamazaki <testtest.email@example.com>
2019-10-07 23:01:25 +0900

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,67 +1,50 @@
 # frozen_string_literal: true
 
-User.create!(name: '山崎　聡之',
-             email: 'b.baller-toshi.1st@ezweb.ne.jp',
-             password: 'aaaaaa',
-             password_confirmation: 'aaaaaa',
-             admin: true)
+User.create!([{ name: '山崎　聡之',
+                email: 'b.baller-toshi.1st@ezweb.ne.jp',
+                password: 'aaaaaa',
+                password_confirmation: 'aaaaaa',
+                admin: true },
+              { name: 'テストユーザー',
+                email: 'sample@example.jp',
+                password: 'aaaaaa',
+                password_confirmation: 'aaaaaa',
+                admin: true }])
 
-Child.create!(name: '山崎　陽祐',
-              sex: '男の子',
-              birthday: '2016-10-13',
-              user_id: 1,
-              child_password: 'TESTES')
+Child.create!([{ name: '山崎　陽祐',
+                 sex: '男の子',
+                 birthday: '2016-10-13',
+                 user_id: 1,
+                 child_password: 'TESTES' },
+               { name: '山崎　ほのか',
+                 sex: '女の子',
+                 birthday: '2019-09-20',
+                 user_id: 1,
+                 child_password: 'SAMPLE' },
+               { name: '太郎(長男)',
+                 sex: '男の子',
+                 birthday: '2015-09-20',
+                 user_id: 2,
+                 child_password: 'TAROUU' },
+               { name: '次郎(次男)',
+                 sex: '男の子',
+                 birthday: '2016-11-10',
+                 user_id: 2,
+                 child_password: 'ZIROUU' },
+               { name: '花子(長女)',
+                 sex: '女の子',
+                 birthday: '2017-08-10',
+                 user_id: 2,
+                 child_password: 'HANAKO' },
+               { name: '由香(次女)',
+                 sex: '女の子',
+                 birthday: '2018-04-06',
+                 user_id: 2,
+                 child_password: 'CHACHA' }])
 
-Child.create!(name: '山崎　ほのか',
-              sex: '女の子',
-              birthday: '2019-09-20',
-              user_id: 1,
-              child_password: 'SAMPLE')
-
-Family.create!(user_id: 1,
-               child_id: 1)
-
-Family.create!(user_id: 1,
-               child_id: 2)
-
-User.create!(name: 'テストユーザー',
-             email: 'sample@example.jp',
-             password: 'aaaaaa',
-             password_confirmation: 'aaaaaa',
-             admin: true)
-
-Child.create!(name: '太郎(長男)',
-              sex: '男の子',
-              birthday: '2015-09-20',
-              user_id: 2,
-              child_password: 'TAROUU')
-
-Child.create!(name: '次郎(次男)',
-              sex: '男の子',
-              birthday: '2016-11-10',
-              user_id: 2,
-              child_password: 'ZIROUU')
-
-Child.create!(name: '花子(長女)',
-              sex: '女の子',
-              birthday: '2017-08-10',
-              user_id: 2,
-              child_password: 'HANAKO')
-
-Child.create!(name: '由香(次女)',
-              sex: '女の子',
-              birthday: '2018-04-06',
-              user_id: 2,
-              child_password: 'CHACHA')
-
-Family.create!(user_id: 2,
-               child_id: 3)
-
-Family.create!(user_id: 2,
-               child_id: 4)
-
-Family.create!(user_id: 2,
-               child_id: 5)
-
-Family.create!(user_id: 2,
-               child_id: 6)
+Family.create!([{ user_id: 1, child_id: 1 },
+                { user_id: 1, child_id: 2 },
+                { user_id: 2, child_id: 3 },
+                { user_id: 2, child_id: 4 },
+                { user_id: 2, child_id: 5 },
+                { user_id: 2, child_id: 6 }])
